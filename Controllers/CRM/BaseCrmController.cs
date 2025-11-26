9656aa19771538321686356bba6585ec1c6a7f7f
Marco Meza Sanchez <marco.meza@insertec.biz>
2025-11-26 15:56:01 +0100

    chore: add test marker in BaseCrmController

--- Controllers/CRM/BaseCrmController.cs
+++ Controllers/CRM/BaseCrmController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Web.Http;
 using IND_CRM_API.Services;
-
+//TEST
 namespace IND_CRM_API.Controllers
 {
     public abstract class BaseCrmController : ApiController
@@ -11,7 +11,7 @@ namespace IND_CRM_API.Controllers
         protected string GetAuthenticatedUsername()
         {
             var username = User?.Identity?.Name;
-            if (string.IsNullOrWhiteSpace(username))
+            if (string.IsNullOrWhiteSpace(username)) 
                 throw new Exception("User not authenticated or invalid token.");
             return username;
         }
